c175425088443599d3b1dcec5375d578ff98f28f
ahmedabougabal <ahmedabougabal@live.com>
2025-01-19 15:38:31 +0200

    fix: update Tailwind configuration for production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  build: {
+    cssMinify: true,
+    cssCodeSplit: true,
   },
 })
